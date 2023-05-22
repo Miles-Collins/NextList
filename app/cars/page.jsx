685aa8fe08d6d23c21d14846e042237b3bfb3b18
Miles-Collins <miles.collins@ymail.com>
2023-05-22 16:59:08 -0600

    sync

---
+++ app/cars/page.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image.js";
+import React from "react";
+
+async function GetCars() {
+  const res = await fetch("https://sandbox.codeworksacademy.com/api/cars");
+
+  if (!res.ok) {
+    throw new Error("Something went wrong with the fetch.");
+  }
+  return res.json();
+  // const results = await res.json();
+  // console.log("CAR RESULTS", results);
+}
+
+function Header() {
+  return <h1 className="text-4xl">To the moon 🚀</h1>;
+}
+
+// useEffect(() => {
+//   getCars();
+// }, []);
+
+export default async function Page() {
+  const carsData = await GetCars();
+
+  const cars = await Promise.all(carsData);
+
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-5 justify-around p-5 ">
+        {cars.map((c) => (
+          <div key={c.id} className="shadow-md">
+            {/* <Image
+              src={c.imgUrl}
+              height={100}
+              width={100}
+              alt="Picture of Car."
+            /> */}
+            <img
+              className="object-cover hover:object-scale-down h-48 w-96"
+              src={c.imgUrl}
+            />
+            <p className="text-lg">
+              {c.make} {c.model}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
