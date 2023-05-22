f57771135816b28f6f80bc07229ed75edc11dc23
Miles-Collins <miles.collins@ymail.com>
2023-05-22 16:59:12 -0600

    sync

---
+++ app/page.jsx
@@ -0,0 +1,36 @@
+export default function Page() {
+
+
+
+  return (
+    <div className="homePage">
+    <div className="container-fluid m-auto grid grid-cols-3">
+      <div className="tile bg-slate-500">
+        <h1 className="tile-marker text-4xl">TailWind Test</h1>
+      </div>
+      <div className="tile bg-yellow-300">
+        <h1 className="tile-marker text-4xl">TailWind Test</h1>
+      </div>
+      <div className="tile bg-red-900">
+        <h1 className="tile-marker text-4xl">TailWind Test</h1>
+      </div>
+      <div className="bg-blue-400">
+        <h1 className="tile-marker text-4xl">TailWind Test</h1>
+      </div>
+      <div>
+        <h1 className="tile-marker text-4xl">TailWind Test</h1>
+      </div>
+      <div>
+        <h1 className="tile-marker text-4xl">TailWind Test</h1>
+      </div>
+    </div><div className="container-fluid m-auto grid grid-cols-2">
+        <div className="tile bg-orange-300">
+          <h1 className="tile-marker text-3xl">TailWind Test</h1>
+        </div>
+        <div className="tile bg-orange-600">
+          <h1 className="tile-marker text-3xl">TailWind Test</h1>
+        </div>
+      </div>
+      </div>
+  )
+}
